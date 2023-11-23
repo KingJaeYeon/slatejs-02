0cfc0bce2aced54530070f5e496c66b1c13897dd
pjy <wodus331@gmail.com>
2023-11-23 18:33:33 +0900

    history implement

--- src/store/editorStore.ts
+++ src/store/editorStore.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { withReact } from "slate-react";
 import { createEditor } from "slate";
+import { withHistory } from "@/components/slate-plugins/with-history";
 
 type State = {
   editor: any;
@@ -12,12 +13,12 @@ type Action = {
 };
 
 const initialState: State = {
-  editor: withReact(createEditor()),
+  editor: withReact(withHistory(createEditor())),
   shift: false,
 };
 
 export const useEditorStore = create<State & Action>()((set, get) => ({
-  editor: withReact(createEditor()),
+  editor: withReact(withHistory(createEditor())),
   shift: false,
   setShift: (shift: boolean) => set({ shift }),
   reset: () => set(initialState),
